f854036a4bb6275e615f4fb3c38e080905369cd3
charanvengaladasu <charanvengaladasu@gmail.com>
2026-07-01 21:56:35 +0530

    python-problems

---
+++ patterns-in-pythons/pattern-4.py
@@ -0,0 +1,8 @@
+#Pattern 4 [Right triangle]
+n=5
+for i in range(n):
+  for j in range(i,n):
+    print(' ',end=' ')
+  for j in range(i+1):
+    print('*',end=' ')
+  print()
---
+++ patterns-in-pythons/pattern-7.py
@@ -0,0 +1,10 @@
+#7.Pattern 7 [Reverse Hill Pattern]
+n=5
+for i in range(n):
+  for j in range(i+1):
+    print(' ',end=' ')
+  for j in range(i,n-1):
+    print('*', end=' ')
+  for j in range(i,n):
+    print('*',end=' ')
+  print()
